2df9751873b2cf46ac90b3cfb872bd32da350703
sriVishalS <vishalS221202@gmail.com>
2023-02-13 17:52:16 +0530

    app

--- js/app.js
+++ js/app.js
@@ -1,8 +1,28 @@
 var myVar;
 function myFunction() {
-  myVar = setTimeout(showPage, 2000);
+  myVar = setTimeout(showPage, 1000);
 }
 
 function showPage() {
   document.querySelector(".loaderWrapper").style.display = "none";
 }
+//! ---------- MAIL CONFIG--------------
+sendEmail = () => {
+  const usrObj2 = {
+    from_name: document.getElementById("cName").value,
+    from_email: document.getElementById("cMail").value,
+    from_number: document.getElementById("cNumber").value,
+    from_number: document.getElementById("cMessage").value,
+  };
+  emailjs.send("service_t1v18zn", "template_hc934en", usrObj2).then((res) => {
+    alert("Mail Reached, We Will Reach You Shortly");
+  });
+};
+subscribe = () => {
+  const usrObj2 = {
+    from_name: document.getElementById("sMail").value,
+  };
+  emailjs.send("service_yfz6djm", "template_sk8z21j", usrObj2).then((res) => {
+    alert("Thanks For Reaching Us! And Subscribed");
+  });
+};
